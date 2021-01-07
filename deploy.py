7352b16016b7dc1a1b4753e871419a396b355960
David Schultz <davids24@gmail.com>
2021-01-07 16:17:37 -0600

    add ubuntu 20.04 for new combo stable and releases after V01-01-00

--- deploy.py
+++ deploy.py
@@ -31,39 +31,51 @@ def build_docker(metaproject, version, target, base_os):
     dockerfile = os.path.join(base_os, metaproject, version, 'Dockerfile')
     #call(['docker', 'pull', full_tag])
     check_call(['docker', 'build', '--pull', '-f', dockerfile, '--target', target, '-t', full_tag, '.'])
-    check_call(['docker', 'push', full_tag])
+    #check_call(['docker', 'push', full_tag])
 
 def retag(old, new):
     full_tag_old = image_name+':'+old
     full_tag_new = image_name+':'+new
     check_call(['docker', 'tag', full_tag_old, full_tag_new])
-    check_call(['docker', 'push', full_tag_new])
+    #check_call(['docker', 'push', full_tag_new])
+
+def get_tags(metaproject, version, base_os):
+    dockerfile = os.path.join(base_os, metaproject, version, 'Dockerfile')
+    tags = []
+    with open(dockerfile) as f:
+        for line in f:
+            parts = line.strip().split()
+            if len(parts) >= 3 and parts[0].lower() == 'from' and parts[2].lower() == 'as':
+                tags.append(parts[-1])
+    return tags
 
 def build_metaproject(metaproject, version, base_os):
     print('now working on '+metaproject+'-'+version+'-XXX-'+base_os)
 
     date = get_date()
 
-    if not skip(metaproject, version, 'install', base_os):
+    tags = get_tags(metaproject, version, base_os)
+
+    if 'install' in tags and not skip(metaproject, version, 'install', base_os):
         build_docker(metaproject, version, 'install', base_os)
         if metaproject == 'combo' and version == 'stable':
             retag(metaproject+'-'+version+'-install-'+base_os, metaproject+'-'+version+'-install-'+base_os+'-'+date)
 
-    if not skip(metaproject, version, 'slim', base_os):
+    if 'slim' in tags and not skip(metaproject, version, 'slim', base_os):
         build_docker(metaproject, version, 'slim', base_os)
         retag(metaproject+'-'+version+'-slim-'+base_os, metaproject+'-'+version+'-slim')
         if metaproject == 'combo' and version == 'stable':
             retag(metaproject+'-'+version+'-slim-'+base_os, version+'-slim')
             retag(metaproject+'-'+version+'-slim-'+base_os, metaproject+'-'+version+'-slim-'+base_os+'-'+date)
 
-    if not skip(metaproject, version, 'prod', base_os):
+    if 'prod' in tags and not skip(metaproject, version, 'prod', base_os):
         build_docker(metaproject, version, 'prod', base_os)
         retag(metaproject+'-'+version+'-prod-'+base_os, metaproject+'-'+version+'-prod')
         if metaproject == 'combo' and version == 'stable':
             retag(metaproject+'-'+version+'-prod-'+base_os, version+'-prod')
             retag(metaproject+'-'+version+'-prod-'+base_os, metaproject+'-'+version+'-prod-'+base_os+'-'+date)
 
-    if not skip(metaproject, version, 'devel', base_os):
+    if 'devel' in tags and not skip(metaproject, version, 'devel', base_os):
         build_docker(metaproject, version, 'devel', base_os)
         retag(metaproject+'-'+version+'-devel-'+base_os, metaproject+'-'+version+'-devel')
         retag(metaproject+'-'+version+'-devel-'+base_os, metaproject+'-'+version)
@@ -73,26 +85,25 @@ def build_metaproject(metaproject, version, base_os):
             retag(metaproject+'-'+version+'-devel-'+base_os, 'latest')
             retag(metaproject+'-'+version+'-devel-'+base_os, metaproject+'-'+version+'-devel-'+base_os+'-'+date)
 
-    if not skip(metaproject, version, 'cuda10.2', base_os):
-        build_docker(metaproject, version, 'cuda10.2', base_os)
-        retag(metaproject+'-'+version+'-cuda10.2-'+base_os, metaproject+'-'+version+'-cuda10.2')
-        retag(metaproject+'-'+version+'-cuda10.2-'+base_os, metaproject+'-'+version+'-cuda')
-        if metaproject == 'combo' and version == 'stable':
-            retag(metaproject+'-'+version+'-cuda10.2-'+base_os, version+'-cuda10.2')
-            retag(metaproject+'-'+version+'-cuda10.2-'+base_os, version+'-cuda')
-            retag(metaproject+'-'+version+'-cuda10.2-'+base_os, metaproject+'-'+version+'-cuda10.2-'+base_os+'-'+date)
+    for t in tags:
+        if 'cuda' in t or 'tensorflow' in t:
+            if not skip(metaproject, version, t, base_os):
+                build_docker(metaproject, version, t, base_os)
+                retag(metaproject+'-'+version+'-'+t+'-'+base_os, metaproject+'-'+version+'-'+t)
+                if 'cuda' in t:
+                    retag(metaproject+'-'+version+'-'+t+'-'+base_os, metaproject+'-'+version+'-cuda')
+                elif 'tensorflow' in t:
+                    retag(metaproject+'-'+version+'-'+t+'-'+base_os, metaproject+'-'+version+'-tensorflow')
+                if metaproject == 'combo' and version == 'stable':
+                    retag(metaproject+'-'+version+'-'+t+'-'+base_os, version+'-'+t)
+                    if 'cuda' in t:
+                        retag(metaproject+'-'+version+'-'+t+'-'+base_os, version+'-cuda')
+                    elif 'tensorflow' in t:
+                        retag(metaproject+'-'+version+'-'+t+'-'+base_os, version+'-tensorflow')
+                    retag(metaproject+'-'+version+'-'+t+'-'+base_os, metaproject+'-'+version+'-'+t+'-'+base_os+'-'+date)
 
-    if not skip(metaproject, version, 'tensorflow.2.1.0', base_os):
-        build_docker(metaproject, version, 'tensorflow.2.1.0', base_os)
-        retag(metaproject+'-'+version+'-tensorflow.2.1.0-'+base_os, metaproject+'-'+version+'-tensorflow.2.1.0')
-        retag(metaproject+'-'+version+'-tensorflow.2.1.0-'+base_os, metaproject+'-'+version+'-tensorflow')
-        if metaproject == 'combo' and version == 'stable':
-            retag(metaproject+'-'+version+'-tensorflow.2.1.0-'+base_os, version+'-tensorflow.2.1.0')
-            retag(metaproject+'-'+version+'-tensorflow.2.1.0-'+base_os, version+'-tensorflow')
-            retag(metaproject+'-'+version+'-tensorflow.2.1.0-'+base_os, metaproject+'-'+version+'-tensorflow.2.1.0-'+base_os+'-'+date)
 
-
-for base_os in ('ubuntu18.04',):
+for base_os in ('ubuntu18.04','ubuntu20.04'):
     for metaproject in os.listdir(base_os):
         for version in os.listdir(os.path.join(base_os, metaproject)):
             build_metaproject(metaproject, version, base_os)
